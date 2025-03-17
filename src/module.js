01df1ec0ccf3c95782799d84c6e55eab1cd71038
daedalus11069 <daedalus969@gmail.com>
2025-03-17 05:19:57 -0700

    More config, less error prone usage

--- src/module.js
+++ src/module.js
@@ -1,49 +1,61 @@
 const spendPoolHandler = async messageId => {
-  if (game.user.isGM) {
-    const message = ChatMessage.get(messageId);
-    const dicePoolsData = await game.settings.get("dicePools", "data");
-    const data = dicePoolsData ? JSON.parse(dicePoolsData) : null;
+  const message = ChatMessage.get(messageId);
+  const dicePoolsData = await game.settings.get("dicePools", "data");
+  const data = dicePoolsData ? JSON.parse(dicePoolsData) : null;
 
-    let fallbackTrigger = game.settings.get(
-      "dice-pool-spender",
-      "fallbackTrigger"
-    );
-    if (fallbackTrigger === "") {
-      fallbackTrigger = "action die";
-    }
+  let fallbackTrigger = game.settings.get(
+    "dice-pool-spender",
+    "fallbackTrigger"
+  );
+  if (fallbackTrigger === "") {
+    fallbackTrigger = "action die";
+  }
 
-    let trigger = game.settings.get("dice-pool-spender", "trigger");
-    if (trigger === "") {
-      trigger = fallbackTrigger;
-    }
-    if (data !== null) {
-      if (typeof message.rolls !== "undefined" && message.rolls.length > 0) {
-        const { rolls } = message;
-        for (const roll of rolls) {
-          for (const dice of roll.dice) {
-            const { flavor } = dice;
-            const triggerIdx = flavor.indexOf(trigger);
-            if (triggerIdx >= 0) {
-              const flavorRest = flavor
-                .slice(triggerIdx + trigger.length)
-                .trim();
-              if (flavorRest !== "") {
-                const possibilities = [];
-                for (const [idx, pool] of data.entries()) {
-                  const reg = new RegExp(pool.name, "i");
-                  if (reg.test(flavorRest)) {
-                    possibilities.push({ idx, name: pool.name });
-                  }
-                }
-                possibilities.sort((a, b) => b.name.length - a.name.length);
-                let dicePoolIdx = -1;
-                if (possibilities.length > 0) {
-                  dicePoolIdx = possibilities[0].idx;
-                }
-                if (dicePoolIdx >= 0) {
-                  dicePools.decreaseDicePool(dicePoolIdx);
-                }
-              }
+  let trigger = game.settings.get("dice-pool-spender", "trigger");
+  if (trigger === "") {
+    trigger = fallbackTrigger;
+  }
+
+  let fallbackBonusTrigger = game.settings.get(
+    "dice-pool-spender",
+    "fallbackBonusTrigger"
+  );
+  if (fallbackBonusTrigger === "") {
+    fallbackBonusTrigger = "+b";
+  }
+
+  let bonusTrigger = game.settings.get("dice-pool-spender", "bonusTrigger");
+  if (bonusTrigger === "") {
+    bonusTrigger = fallbackBonusTrigger;
+  }
+
+  let bonusPoolSuffix = game.settings.get(
+    "dice-pool-spender",
+    "bonusPoolSuffix"
+  );
+  if (bonusPoolSuffix === "") {
+    bonusPoolSuffix = "bonus";
+  }
+
+  if (data !== null) {
+    if (typeof message.rolls !== "undefined" && message.rolls.length > 0) {
+      const { rolls } = message;
+      for (const roll of rolls) {
+        for (const dice of roll.dice) {
+          const { flavor } = dice;
+          const triggerIdx = flavor.indexOf(trigger);
+          if (triggerIdx >= 0) {
+            const flavorRest = flavor.slice(triggerIdx + trigger.length).trim();
+            let speakerAlias = message.speaker.alias;
+            if (flavorRest.indexOf(bonusTrigger) >= 0) {
+              speakerAlias += ` ${bonusPoolSuffix}`;
+            }
+            speakerAlias = speakerAlias.toLowerCase();
+            const dicePoolIdx = data.findIndex(
+              ({ name }) => name.toLowerCase() === speakerAlias
+            );
+            if (dicePoolIdx >= 0) {
+              dicePools.decreaseDicePool(dicePoolIdx);
             }
           }
         }
@@ -59,7 +71,7 @@ Hooks.once("socketlib.ready", () => {
   socket.register("spendPool", spendPoolHandler);
 });
 
-Hooks.once("init", () => {
+Hooks.once("ready", () => {
   game.settings.register("dice-pool-spender", "fallbackTrigger", {
     scope: "world",
     config: true,
@@ -67,6 +79,22 @@ Hooks.once("init", () => {
     hint: "DICEPOOLSPENDER.global.FallbackTrigger.Hint",
     type: String,
     default: "action die"
+  });
+  game.settings.register("dice-pool-spender", "fallbackBonusTrigger", {
+    scope: "world",
+    config: true,
+    name: "DICEPOOLSPENDER.global.FallbackBonusTrigger.Name",
+    hint: "DICEPOOLSPENDER.global.FallbackBonusTrigger.Hint",
+    type: String,
+    default: "+b"
+  });
+  game.settings.register("dice-pool-spender", "bonusPoolSuffix", {
+    scope: "world",
+    config: true,
+    name: "DICEPOOLSPENDER.global.BonusPoolSuffix.Name",
+    hint: "DICEPOOLSPENDER.global.BonusPoolSuffix.Hint",
+    type: String,
+    default: "bonus"
   });
   game.settings.register("dice-pool-spender", "trigger", {
     scope: "client",
@@ -76,18 +104,27 @@ Hooks.once("init", () => {
     type: String,
     default: ""
   });
-});
+  game.settings.register("dice-pool-spender", "bonusTrigger", {
+    scope: "client",
+    config: true,
+    name: "DICEPOOLSPENDER.local.BonusTrigger.Name",
+    hint: "DICEPOOLSPENDER.local.BonusTrigger.Hint",
+    type: String,
+    default: ""
+  });
 
-Hooks.once("ready", () => {
-  if (game.user.isGM) {
-    if (typeof game.modules.get("dice-so-nice") !== "undefined") {
-      Hooks.on("diceSoNiceRollComplete", async messageId => {
+  if (typeof game.modules.get("dice-so-nice") !== "undefined") {
+    Hooks.on("diceSoNiceRollComplete", async messageId => {
+      const message = ChatMessage.get(messageId);
+      if (game.user.id === message.user.id) {
         await socket.executeAsGM("spendPool", messageId);
-      });
-    } else {
-      Hooks.on("renderChatMessage", async message => {
+      }
+    });
+  } else {
+    Hooks.on("renderChatMessage", async message => {
+      if (game.user.id === message.user.id) {
         await socket.executeAsGM("spendPool", message.id);
-      });
-    }
+      }
+    });
   }
 });
